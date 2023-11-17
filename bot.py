b76bc401ffa9aae4509a98306818bca1b763d988
Piqueiras <danipiquerasvalle@gmail.com>
2023-11-17 20:31:05 +0100

    Coloringos inicial

--- bot.py
+++ bot.py
@@ -86,7 +86,23 @@ async def racha(interaction: discord.Interaction):
         return
     
     await interaction.response.send_message(f"Llevas una racha de {racha} desde el dia {fecha}")
-    
+
+color = {
+    'rojo':1,
+    'pistacho':2,
+    'oro':3,
+    'azul':4,
+    'rosa':5,
+    'cian':6,
+    'blanco':7
+}
+
+formato = {
+    'normal':0,
+    'negrita':1,
+    'subrayado':4
+}
+
 @tree.command(name="calendario",description="Muestra info de los días que has estudiado")
 @discord.app_commands.describe(ano="Año")
 async def calendario(interaction: discord.Interaction, ano: int):
@@ -94,10 +110,9 @@ async def calendario(interaction: discord.Interaction, ano: int):
                                                                              database.user_yearly_dates(config.JSON_PATH,
                                                                                                         interaction.user.name,
                                                                                                         ano),
-                                                                             '[2;45m[2;37m',
-                                                                             '[0m[2;45m[0m')+"```")
-    #Lmfao que son estos codigos de color
-    
+                                                                             f'\u001b[{formato["subrayado"]};3{color["oro"]}m',
+                                                                             '\u001b[0m')+"```")
+#Confia bro
 
 keepAlive()
 client.run(config.TOKEN)
